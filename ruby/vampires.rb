7316633d6b390ce09164e0d42fb544d55625c65e
Lia Hogan <dahogan131@gmail.com>
2017-01-26 15:41:35 -0800

    Inserted while loop for allergies and made edits to remove bugs for list of vampire names.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,6 +12,7 @@ p vampires_like_garlic
 p !vampires_like_garlic
 p !(wolves_like_sunshine && wolves_like_garlic)
 
+
 emp_ct = 3
 until emp_ct == 0
   p "Hello! What is your name?"
@@ -28,11 +29,27 @@ until emp_ct == 0
   p "#{user_name}, did you want to enroll in our company healthcare plan?"
   hmo_answer = gets.chomp
 
+  p "Any allergies?"
+  alg_answer = gets.chomp
+
+  alg_end = "done"
+  app_answer = gets.chomp
+  while app_answer != alg_end
+  	p "Any other allergies? If not, please enter 'done'!"
+  	app_answer = gets.chomp
+  end
+
   p "Thank you for your honest responses, #{user_name}!"
-  if user_name == ("Dracula") || ("Nosforatu") || ("Chocula")
+  if user_name == "Dracula" || user_name == "Chocula" || user_name == "Nosforatu"
 	vamp_name = true
   else
 	vamp_name = false
+  end
+
+  if alg_answer == "sunshine"
+  	vamp_allergy = true
+  else
+  	vamp_allergy = false
   end
 
   if user_age > 120
@@ -57,7 +74,7 @@ until emp_ct == 0
 	p "Definitely a vampire."
   elsif (vamp_age == true && vamp_answer == true && vamp_hmo == true)
 	p "Almost certainly a vampire"
-  elsif vamp_age == true && (vamp_answer == true || vamp_hmo == true)
+  elsif (vamp_age == true && (vamp_answer == true || vamp_hmo == true)) || vamp_allergy == true
 	p "Probably a vampire"
   elsif (vamp_age == false && vamp_name == false && vamp_answer == false && vamp_hmo == false)
 	p "Probably not a vampire."
@@ -66,7 +83,8 @@ until emp_ct == 0
   end
   emp_ct = emp_ct - 1
 end
-
+ 
+ p "Actually, never mind. Forgot The Dark Ages ended long ago, and our firms vacancies are open to all qualified walks of life, vampires included!"
 
 
 
