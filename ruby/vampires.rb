5629087af9048fb10cf341d11bb9d547b201bcec
Lia Hogan <dahogan131@gmail.com>
2017-01-26 12:29:04 -0800

    Created loop that asks questionnare to 3 candidates one at a time.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,57 +12,104 @@ p vampires_like_garlic
 p !vampires_like_garlic
 p !(wolves_like_sunshine && wolves_like_garlic)
 
-p "Hello! What is your name?"
-user_name = gets.chomp
-p "Hello #{user_name}!"
+emp_ct = 3
+until emp_ct == 0
+  p "Hello! What is your name?"
+  user_name = gets.chomp
+  p "Hello #{user_name}!"
 
-p "How old are you #{user_name}?"
-user_age = gets.to_i
-p "#{user_name}, you are #{user_age}."
+  p "How old are you #{user_name}?"
+  user_age = gets.to_i
+  p "#{user_name}, you are #{user_age}."
 
-p "#{user_name}, lunch today is spaggetti. Did you want garlic bread with that?"
-user_answer = gets.chomp 
+  p "#{user_name}, lunch today is spaggetti. Did you want garlic bread with that?"
+  user_answer = gets.chomp 
 
-p "#{user_name}, did you want to enroll in our company healthcare plan?"
-hmo_answer = gets.chomp
+  p "#{user_name}, did you want to enroll in our company healthcare plan?"
+  hmo_answer = gets.chomp
 
-p "Thank you for your honest responses, #{user_name}!"
-
-
-
-if user_name == ("Dracula" || "Nosforatu" || "Chocula")
+  p "Thank you for your honest responses, #{user_name}!"
+  if user_name == ("Dracula" || "Nosforatu" || "Chocula")
 	vamp_name = true
-else
+  else
 	vamp_name = false
-end
+  end
 
-if user_age > 120
+  if user_age > 120
 	vamp_age = true
-else
+  else
 	vamp_age = false
-end
+  end
 
-if user_answer == "no"
+  if user_answer == "no"
 	vamp_answer = true
-else
+  else
 	vamp_answer = false
-end
+  end
 
-if hmo_answer == "no"
+  if hmo_answer == "no"
 	vamp_hmo = true
-else
+  else
 	vamp_hmo = false
+  end
+
+  if vamp_name == true
+	p "Definitely a vampire."
+  elsif (vamp_age == true && vamp_answer == true && vamp_hmo == true)
+	p "Almost certainly a vampire"
+  elsif vamp_age == true && (vamp_answer == true || vamp_hmo == true)
+	p "Probably a vampire"
+  elsif (vamp_age == false && vamp_name == false && vamp_answer == false && vamp_hmo == false)
+	p "Probably not a vampire."
+  else
+	p "Dunno... could be... try and put 'em in the sun."
+  end
+  emp_ct = emp_ct - 1
 end
 
-if vamp_name == true
+
+  if user_name == ("Dracula" || "Nosforatu" || "Chocula")
+	vamp_name = true
+  else
+	vamp_name = false
+  end
+
+  if user_age > 120
+	vamp_age = true
+  else
+	vamp_age = false
+  end
+
+  if user_answer == "no"
+	vamp_answer = true
+  else
+	vamp_answer = false
+  end
+
+  if hmo_answer == "no"
+	vamp_hmo = true
+  else
+	vamp_hmo = false
+  end
+
+  if vamp_name == true
 	p "Definitely a vampire."
-elsif (vamp_age == true && vamp_answer == true && vamp_hmo == true)
+  elsif (vamp_age == true && vamp_answer == true && vamp_hmo == true)
 	p "Almost certainly a vampire"
-elsif vamp_age == true && (vamp_answer == true || vamp_hmo == true)
+  elsif vamp_age == true && (vamp_answer == true || vamp_hmo == true)
 	p "Probably a vampire"
-elsif (vamp_age == false && vamp_name == false && vamp_answer == false && vamp_hmo == false)
+  elsif (vamp_age == false && vamp_name == false && vamp_answer == false && vamp_hmo == false)
 	p "Probably not a vampire."
-else
+  else
 	p "Dunno... could be... try and put 'em in the sun."
-end
+  end
+
+
+#An until loop makes the most sense here. 
+#Thinking, assign each employee a number. 
+#If/else inside the until. 
+
+
+
+
 	
