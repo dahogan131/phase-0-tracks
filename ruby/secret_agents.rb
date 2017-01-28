c055dfa8a0042e432bf4589dc10892bb23bb38b1
Lia Hogan <dahogan131@gmail.com>
2017-01-27 21:14:17 -0800

    better code for moving each letter in string forward one letter

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,12 +1,5 @@
-p "lia"
-p "tod"
-p "tim"
-p "joe"
-
-def enc_name(arg1)
-  p arg1[0].next + arg1[1].next + arg1[2].next
+name = "bigfoot"
+name = name.gsub(/./) do |word|
+  word.next
 end
-enc_name("lia")
-enc_name("tod")
-enc_name("tim")
-enc_name("joe")
+puts name
