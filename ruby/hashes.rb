3d950cd2be1e75c53c24a5ed7649fba851417808
Lia Hogan <dahogan131@gmail.com>
2017-01-31 14:27:01 -0800

    Initial commit. Also drafted the pseudocode for the program.

---
+++ ruby/hashes.rb
@@ -0,0 +1,5 @@
+# The value of the keys will be a variable that has a gets.chomp value. 
+# The answer variable to each question will be gets.chomp and that answer variable will be the value for the key in the hash.
+
+
+p
