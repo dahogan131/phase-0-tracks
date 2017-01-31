0d13788e7f2ca575da0ebd8c291aae4947dd89d0
Lia Hogan <dahogan131@gmail.com>
2017-01-31 12:28:38 -0800

    Inserted hash, with filled out answers to questions 1 - 5 on the application.

---
+++ ruby/interior.rb
@@ -0,0 +1,7 @@
+app_input = {
+	"Name:" => "Lia",
+	"Address:" => "22 Black Lion Rd, Westchestershire on Barnsbey",
+	"Email:" => "lia_programmer@gmail.com",
+	"Phone:" => "867-5309",
+	"Fave shade of blue:" => "navy",
+}
