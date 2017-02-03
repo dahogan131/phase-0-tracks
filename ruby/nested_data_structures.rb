b102685ddb145971ea286390a94562f8204c6dee
Lia Hogan <dahogan131@gmail.com>
2017-02-03 09:32:16 -0800

    Did previous commit on other file oops. Debugged data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,16 @@
+enchanted_forest = {
+	keebler: {
+		village: 'Keebler Treehouse', 
+		inhabitants: ['Tuck', 'Elmer', 'Zack']
+		},
+	smurf: {
+		village: 'Smurf Village', 
+		inhabitants:  ['Papa Smurf', 'Smurfette', 'Brainy Smurf']
+		},
+	troll: {	
+		village: 'Troll House', 
+		inhabitants:  ['Big Troll', 'Happy Troll', 'Garden Troll', 'Internet Troll']
+		}
+}
+
+p enchanted_forest[:keebler][:inhabitants][0]
