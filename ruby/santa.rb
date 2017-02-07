022bf961f531029e2c0cda91e3a3ca0e19ca741c
Lia Hogan <dahogan131@gmail.com>
2017-02-06 19:39:37 -0800

    Implemented Release 0 methods. Two methods that print strings, one method that prints a string and accepts an argument.

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+def speak
+	p "Ho Ho Ho! Happy Hannukah!"
+end
+
+p speak
+
+def eat_milk_and_cookies(cookie)
+	p "Oh man, friggin' loved that #{cookie}!"
+end
+
+p eat_milk_and_cookies("gingersnap")
+
+def initialize
+	p "Initializing Santa sequence... "
+end
+
+p initialize
