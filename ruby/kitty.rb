22cc6ff058a0b629454198690bb348b4cb281755
Lia Hogan <dahogan131@gmail.com>
2017-02-06 17:28:07 -0800

    Inserted String.methods and str.methods to compare the difference between methods for classes and instances.

--- ruby/kitty.rb
+++ ruby/kitty.rb
@@ -15,6 +15,18 @@ p osiris.instance_of?(Array)
 p oscar.instance_of?(Kitty)
 p arr.instance_of?(Array)
 
+str = String.new
+p str.length
+p str.empty?
+str += "huh."
+p str
+greeting = String.new("hello")
+p greeting.length
+p greeting.upcase
+
+p String.methods
+p greeting.upcase 
+p str.methods
 
 
 
