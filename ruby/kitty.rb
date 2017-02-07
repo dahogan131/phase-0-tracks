db8648c2c8624659f36ce4dfe575278a4cc33ba4
Lia Hogan <dahogan131@gmail.com>
2017-02-06 17:08:35 -0800

    Did release 1. Practiced some basics with instances in a class.

--- ruby/kitty.rb
+++ ruby/kitty.rb
@@ -1,4 +1,20 @@
 class Kitty
 end
 
-p Kitty.methods
+arr = ["a", "r", "r"]
+
+ Kitty.methods
+
+felix = Kitty.new
+oscar = Kitty.new
+osiris = Kitty.new
+
+p felix.class
+p oscar == osiris
+p osiris.instance_of?(Array)
+p oscar.instance_of?(Kitty)
+p arr.instance_of?(Array)
+
+
+
+
