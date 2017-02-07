ba301570277c4290a35ad003f9b2353c6a6cd9aa
Lia Hogan <dahogan131@gmail.com>
2017-02-06 19:51:00 -0800

    Put methods in a class.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,17 +1,14 @@
-def speak
-	p "Ho Ho Ho! Happy Hannukah!"
+class Santa
+	def speak
+		p "Ho Ho Ho! Happy Hannukah!"
+	end
+	# p speak
+	def eat_milk_and_cookies(cookie)
+		p "Oh man, friggin' loved that #{cookie}!"
+	end
+	# p eat_milk_and_cookies("gingersnap")
+	def initialize
+		p "Initializing Santa sequence... "
+	end
+	# p initialize
 end
-
-p speak
-
-def eat_milk_and_cookies(cookie)
-	p "Oh man, friggin' loved that #{cookie}!"
-end
-
-p eat_milk_and_cookies("gingersnap")
-
-def initialize
-	p "Initializing Santa sequence... "
-end
-
-p initialize
