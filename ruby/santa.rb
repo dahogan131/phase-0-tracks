f82cb20b32114151d802d000bf57bdd9625f94d5
Lia Hogan <dahogan131@gmail.com>
2017-02-06 20:58:43 -0800

    Made it possible to initialize instances in the class.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,24 @@
+# class Santa
+# 	def speak
+# 		p "Ho Ho Ho! Happy Hannukah!"
+# 	end
+# 	# p speak
+# 	def eat_milk_and_cookies(cookie)
+# 		p "Oh man, friggin' loved that #{cookie}!"
+# 	end
+# 	# p eat_milk_and_cookies("gingersnap")
+# 	def initialize
+# 		p "Initializing Santa sequence... "
+# 	end
+# 	# p initialize
+# end
+
+
+
+# p speak 
+# p eat_milk_and_cookies("gingersnap")
+# p initialize
+# Santa = Class.new do
 class Santa
 	def speak
 		p "Ho Ho Ho! Happy Hannukah!"
@@ -7,8 +28,14 @@ class Santa
 		p "Oh man, friggin' loved that #{cookie}!"
 	end
 	# p eat_milk_and_cookies("gingersnap")
-	def initialize
+	def initializing
 		p "Initializing Santa sequence... "
 	end
 	# p initialize
 end
+a = Santa.new
+a.eat_milk_and_cookies("cookie")
+a.initializing
+a.speak
+# a.speak
+# a.initialize
