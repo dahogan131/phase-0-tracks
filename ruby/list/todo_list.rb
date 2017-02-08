7f9ea820aa151d43103ca0e626c24852d48b555b
Bigfoot <dahogan131@gmail.com>
2017-02-08 10:45:44 -0800

    Wrote initialize and instance methods and then tested individually with rspec.

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,41 @@
+# class TodoList
+# 	def initialize(items = [])
+# 		@items = items
+# 	end
+
+# 	def get_items(index)
+# 		@items.at(index)
+# 	end
+
+# end
+
+# items = ["do the dishes", "mow the lawn"]
+
+class TodoList
+
+
+    def initialize(items = [])
+        @items = items
+        items = ["do the dishes", "mow the lawn"]   
+    end
+
+    def get_items
+    	@items
+    end
+
+    def add_item(item)
+    	@items << item 
+    end
+
+    def delete_item(item)
+    	@items.delete(item)
+    	item
+    end
+
+    def get_item(index)
+        @items.at(index)
+    end
+
+end
+
+#Driver Code
