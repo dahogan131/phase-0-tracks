1fcf5041afcf0ac2a8274bc826508075181b7d49
Bigfoot <dahogan131@gmail.com>
2017-02-07 16:42:45 -0800

    Pushed instances into array data structured and then pushed said data structures into a hash. Initialize method will not participate in the until loop.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,31 +7,55 @@ class Puppy
 
 end
 
-# def speak(integer, bark)
-# 	puts integer * bark
-# end
 
-# puts speak(7, "woof")
 class Class
-	def method
-		p "okay b"
+	def initialize
+		p "Initializing class sequence..."
 	end
-# index = 0
-# until index == 50
-# 	p method
-# 	index = index + 1
-# end
-method
+
+	def method1
+		p "This is method 1"
+	end
+
+	def method2
+		p "This is method 2"
+	end
+
+#method1
 end
 
 b = Class.new
 #Declare new hash
 #Hash needs keys for each of the methods: Initialize and 2 instance methods
+arr_init = Array.new
+arr_init = []
+
+arr_m1 = Array.new
+arr_m1 = []
+
+arr_m2 = Array.new
+arr_m2 = []
 index = 0
 until index == 50
-	p b
+	#initialize
+	arr_init << initialize
+	arr_m1 << b.method1
+	arr_m2 << b.method2
+
 	#Push the instance of the class into the hash
 	#Somehow make it so that the methods go into their respective keys
 
 	index = index + 1
 end
+
+# p arr_init
+# p arr_m1
+# p arr_m2
+
+data_hash = {
+	:initial => arr_init,
+	:method_1 => arr_m1,
+	:method_2 => arr_m2
+}
+
+p data_hash
