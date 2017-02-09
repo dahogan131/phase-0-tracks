c649456911993faafff8e45bcd94410567575679
Lia Hogan <dahogan131@gmail.com>
2017-02-08 16:01:30 -0800

    Got program to convert index numbers from loop into martian years and dog years.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -22,32 +22,35 @@ class Class
 	index = 0
 	until index == 50
 		def initialize
-			#p "Initializing class sequence..."
+			p "Initializing dog year sequence..."
 		end
 
-		def method1
+		def dog_years
 			#p "This is method 1"
 			7
 		end
 
-		def method2
+		def martian_years
 			#p "This is method 2"
+			0.05
 		end
 		arr_init << initialize
-		arr_m1 << b.method1
-		arr_m2 << b.method2
+		arr_m1 << b.dog_years
+		arr_m2 << b.martian_years
 		data_hash = {
 		:initial => arr_init,
-		 :method_1 => arr_m1,
-		 :method_2 => arr_m2
+		 :dog_yrs => arr_m1,
+		 :mars_yrs => arr_m2
 	}
 	
 	 data_hash.each do 
-	  p "#{index} * #{Class.method1} ="
-	  p index * method1
+	  p "Dog Years: #{index} * #{Class.dog_years} ="
+	  p index * dog_years
+	  p "Martian Years: #{index} * #{Class.martian_years}"
+	  p index * martian_years
 	  initialize
-	  p Class.method1
-	  p Class.method2
+	  p Class.dog_years
+	  p Class.martian_years
 	 end
 	 
 		index = index + 1
@@ -109,55 +112,55 @@ end
 # p data_hash
 
 
-class Class
-	b = Class.new
+# class Class
+# 	b = Class.new
 
-	arr_init = Array.new
-	arr_init = []
+# 	arr_init = Array.new
+# 	arr_init = []
 
-	arr_m1 = Array.new
-	arr_m1 = []
+# 	arr_m1 = Array.new
+# 	arr_m1 = []
 
-	arr_m2 = Array.new
-	arr_m2 = []
-	index = 0
-	until index == 50
-		def initialize
-			p "Initializing class sequence..."
-		end
+# 	arr_m2 = Array.new
+# 	arr_m2 = []
+# 	index = 0
+# 	until index == 50
+# 		def initialize
+# 			p "Initializing class sequence..."
+# 		end
 
-		def method1
-			p "This is method 1"
-		end
+# 		def method1
+# 			p "This is method 1"
+# 		end
 
-		def method2
-			p "This is method 2"
-		end
-		arr_init << initialize
-		arr_m1 << b.method1
-		arr_m2 << b.method2
-		index = index + 1
-	end
-	data_hash = {
-		:initial => arr_init,
-		:method_1 => arr_m1,
-		:method_2 => arr_m2
-	}
+# 		def method2
+# 			p "This is method 2"
+# 		end
+# 		arr_init << initialize
+# 		arr_m1 << b.method1
+# 		arr_m2 << b.method2
+# 		index = index + 1
+# 	end
+# 	data_hash = {
+# 		:initial => arr_init,
+# 		:method_1 => arr_m1,
+# 		:method_2 => arr_m2
+# 	}
 
-	# p data_hash
-	# data_hash.each do |hash|
-	# 	puts "#{hash} is an #{index}"
-	# end
-	data_hash[:initial].each do |structure|
-		puts "#{structure} is #{index}"
-	end
-	data_hash[:method_1].each do |structure|
-		puts "#{structure} is #{index}"
-	end
-	data_hash[:method_2].each do |structure|
-		puts "#{structure} is #{index}"
-	end
-end
+# 	# p data_hash
+# 	# data_hash.each do |hash|
+# 	# 	puts "#{hash} is an #{index}"
+# 	# end
+# 	data_hash[:initial].each do |structure|
+# 		puts "#{structure} is #{index}"
+# 	end
+# 	data_hash[:method_1].each do |structure|
+# 		puts "#{structure} is #{index}"
+# 	end
+# 	data_hash[:method_2].each do |structure|
+# 		puts "#{structure} is #{index}"
+# 	end
+# end
 
 # b = Class.new
 
