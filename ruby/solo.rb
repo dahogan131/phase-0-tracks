99e05cc628d8418af3959f5cbd99f8093262abce
Lia Hogan <dahogan131@gmail.com>
2017-02-11 19:33:31 -0800

    Created while loop with conditional for guessing letters to the secret word.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -11,22 +11,54 @@
   # Then the ["_", "_", "_"] array will be put back together into string form. 
 #congratulatory message if they lose, taunting message if they win. 
 
+# times = 0 
+# index = 0
+# word = "camel"
+# user_input = gets.chomp
+# while times != word.length
+#   correct_letters = "_" * word.length
+#   correct_letters = correct_letters.chars
+#   word = word.chars
+#   p word[index]
+#   p correct_letters
+#   p correct_letters.class
+#   p "guess the word"
+#   correct_letters = correct_letters.join
+#   word = word.join
+#   user_input = gets.chomp
+#   index +=1 
+#   times +=1 
+# end
+# p "done"
+
 times = 0 
-index = 0
-word = "camel"
-user_input = gets.chomp
+index = 0 
+word = ["c", "a", "m", "e", "l"]
+letter_answer = ["_", "_", "_", "_", "_"]
 while times != word.length
-  correct_letters = "_" * word.length
-  correct_letters = correct_letters.chars
-  word = word.chars
-  p word[index]
-  p correct_letters
-  p correct_letters.class
-  p "guess the word"
-  correct_letters = correct_letters.join
-  word = word.join
-  user_input = gets.chomp
-  index +=1 
-  times +=1 
+p "guess a letter"
+user_input = gets.chomp 
+# Consider if/else being the true/false for the method in determining guess count...  
+  if user_input == word[0] 
+    letter_answer.delete_at(0)
+    letter_answer.insert(0, word[0])
+    p letter_answer.join
+  elsif user_input == word[1] 
+    letter_answer.delete_at(1)
+    letter_answer.insert(1, word[1])
+    p letter_answer.join
+  elsif user_input == word[2] 
+    letter_answer.delete_at(2)
+    letter_answer.insert(2, word[2])
+    p letter_answer.join
+  elsif user_input == word[3] 
+    letter_answer.delete_at(3)
+    letter_answer.insert(3, word[3])
+    p letter_answer.join
+  elsif user_input == word[4] 
+    letter_answer.delete_at(4)
+    letter_answer.insert(4, word[4])
+    p letter_answer.join
+  end
+  times += 1
 end
-p "done"
