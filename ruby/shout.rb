63328df5ad4abf774813294fb853cedbe1d14edb
Lia Hogan <dahogan131@gmail.com>
2017-02-12 12:15:43 -0800

    inserted methods into shout module and method calls for each method (shout_angrily and shout_happily)

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+  	words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+  	words + "!!!" + " :DDDDD"
+  end
 end
+ 
+p Shout.yell_angrily("GET OFF MY LAWN")
+p Shout.yelling_happily("WELCOME TO THE PARTY!")
