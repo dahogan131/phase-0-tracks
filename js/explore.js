20051405fab1b419d088ac9c2a41bcd29f6dc4f6
Lia Hogan <dahogan131@gmail.com>
2017-02-15 12:40:37 -0800

    Wrote basic pseudocode for reverse method.

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+//Establish a function that accepts a string as a parameter.
+//The function will be composed of a loop that runs until the index of the string parameter is equal to zero.
+	//In the loop, the index of the string decreases by 1 each loop
+	//In the loop there is a variable that represents an empty string.
+		//Each loop cycle, the character of the string index is added to the empty string.
