8270307768a4cac44ce1e362c949cebdaf024ed2
Lia Hogan <dahogan131@gmail.com>
2017-02-15 08:23:57 -0800

    Problem solving.

--- ruby/4.6_practice.rb
+++ ruby/4.6_practice.rb
@@ -1,14 +1,26 @@
-#able to get 2 methods to work with each other by using return values.
+#able to get 2 methods to work with each other because ruby uses implicit return.
+#return is implied
 def return_method
-	return 7
+	7
+	#return 7
+	#puts 7
 end
 
 a = return_method
+p a
 
 def return_method_2
-	return 8 
+	8
+	#return 8 
+	#puts 8
 end
 
 b = return_method_2
-
+p b
 p a + b
+
+def make_array(arg1)
+	array = arg1.chars
+	return array
+end 
+make_array("lia")
