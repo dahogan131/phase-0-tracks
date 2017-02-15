612a7ed1e483840fced49879c2b36709f0cba6bd
Lia Hogan <dahogan131@gmail.com>
2017-02-15 14:16:33 -0800

    Established function that reverses letters in a string. It is character-length specific, however.

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,16 @@
 	//In the loop, the index of the string decreases by 1 each loop
 	//In the loop there is a variable that represents an empty string.
 		//Each loop cycle, the character of the string index is added to the empty string.
+
+//This function will do it, but it's string length-specific. Want to incorporate the loop so that any word can be input
+
+function reverse(string) {
+return string[4] + string[3] + string[2] + string[1] + string[0];
+}
+console.log(reverse("camel"));
+
+//This is the loop I would like to incorporate into my function. 
+
+var string = "string"
+
+for (var index = string.length -1; index > -1; index -=1) console.log(string[index])
