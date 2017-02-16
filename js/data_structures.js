cf1780fe52a0d3c1c562612e2b31956dd1ed83fa
Lia <dahogan131@gmail.com>
2017-02-16 10:45:56 -0800

    Created pseudocode for creating object. Created colors and horse arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,20 @@
+//Assign each horse in the array as a key,
+//Assign each color in the array as a value.
+	//First run conditional for if colors.length = horse.length
+		//Establish empty object
+		//For loop to go through each index of horse and color arrays.
+			//In the for loop, push key and value into empty object. 
+				//key = horse[i], value = 'colors'[i]
+
+
+var colors = ["blue", "red", "orange", "purple"]
+var horse = ["Rocky", "Fin", "Ed", "Martha"]
+
+colors.push("green")
+horse.push("Hass")
+console.log(colors)
+console.log(horse)
+
+var horseColors = {
+	
+}
