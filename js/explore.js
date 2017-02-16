8d11d7acb8a84de6dad9b00cdf5e817060353dcd
Lia <dahogan131@gmail.com>
2017-02-15 22:07:50 -0800

    Implemented loop into function and debugged the program.

--- js/explore.js
+++ js/explore.js
@@ -5,15 +5,23 @@
 		//Each loop cycle, the character of the string index is added to the empty string.
 		//The empty string 
 
-//This function will do it, but it's string length-specific. Want to incorporate the loop so that any word can be input
+//=============This function will do it, but it's string length-specific. Want to incorporate the loop so that any word can be input
 
+//function reverse(string) {
+//return string[4] + string[3] + string[2] + string[1] + string[0];
+//}
+//console.log(reverse("camel"));
+
+//========================This is the loop I would like to incorporate into my function========================================================
+
+//var string = "string"
+//for (var index = string.length -1; index > -1; index -=1) console.log(string[index])
+
+// This function will take any string as an argument. Was able to integrate the while loop into the the function concept.
 function reverse(string) {
-return string[4] + string[3] + string[2] + string[1] + string[0];
+  empty = "";
+  for (var index = string.length -1; index > -1; index -=1) 
+  empty += string[index];
+  return empty
 }
 console.log(reverse("camel"));
-
-//This is the loop I would like to incorporate into my function. 
-
-var string = "string"
-
-for (var index = string.length -1; index > -1; index -=1) console.log(string[index])
