d1e19864cb5f30794e9c48cd1126d1ee7acd85c5
Lia <dahogan131@gmail.com>
2017-02-24 16:10:29 -0800

    Create tables for frisbee players and teams. Establish method for creating teams.

---
+++ databases/kitten_maker/build.rb
@@ -0,0 +1,44 @@
+
+
+require 'sqlite3'
+require 'faker'
+
+db = SQLite3::Database.new("ultimate_frisbee.db")
+db.results_as_hash = true
+
+create_player_table = <<-SQL 
+	CREATE TABLE IF NOT EXISTS players(
+		id INTEGER PRIMARY KEY, 
+		name VARCHAR (255), 
+		age INT, 
+		team_id INT,
+		FOREIGN KEY (team_id) REFERENCES teams(id)
+	)
+SQL
+
+create_teams_table = <<-SQL 
+	CREATE TABLE IF NOT EXISTS teams(
+		id INTEGER PRIMARY KEY,
+		team_name VARCHAR (255)
+	)
+SQL
+
+db.execute(create_teams_table)
+db.execute(create_player_table)
+
+def create_team(db, team_name)
+	db.execute("INSERT INTO teams (team_name) VALUES (?)", [team_name])
+end
+
+2.times do |team_name|
+	puts "Please Enter Team Name:"
+	team_name = gets.chomp
+	create_team(db, team_name)
+end
+
+# def create_player(db, name, age, team_id)
+
+# end
+
+
+
