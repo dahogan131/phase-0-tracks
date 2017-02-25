5f0d9f38ce9d6844062f0a5fd87f812822c5a315
Lia <dahogan131@gmail.com>
2017-02-25 12:32:37 -0800

    designed user interface for game outcome method.

--- databases/kitten_maker/build.rb
+++ databases/kitten_maker/build.rb
@@ -44,6 +44,10 @@ def create_players(db, name, age, team_id)
 	db.execute("INSERT INTO players (name, age, team_id) VALUES (?, ?, ?)", [name, age, team_id])
 end
 
+def game_outcome(db, win, team_comment, team_id)
+	db.execute("INSERT INTO outcome (win, team_comment, team_id) VALUES (?, ?, ?)", [win, team_comment, team_id])
+end
+
 user_input = ""
 while user_input != "done"
 # 2.times do |team_name|
@@ -68,6 +72,36 @@ while user_input2 != "done"
   	puts "Press any key and enter to continue. Enter 'done' when finished."
   	user_input2 = gets.chomp
 end
+
+puts "3, 2, 1, ULTIMATE!"
+
+puts "Sportscaster 1: Wow, look at that disc zippin' on by back and forth!"
+
+puts "Sportscaster 2: Truly a remarkable game of College Ultimate Frisbee!"
+
+puts "Sportscaster 1: The thrill of the game almost makes you forget the crippling amount of student debt these players will be carrying for years after this game!"
+
+puts "Sportscaster 2: Shush, it's frisbee time!"
+
+puts "3... 2... 1... EHHHHHHH, GAME!"
+
+puts "Great game! Or was it? Enter 'yes' or 'no'"
+user_input = gets.chomp
+	if user_input == "yes" 
+		win = true 
+		puts "Any comment your team would like to make on this galiant victory?"
+		team_comment = gets.chomp
+	else 
+		win = false 
+		puts "Any comment your team would like to make on this crushing defeat?"
+		team_comment = gets.chomp
+	end
+puts "What was your Team ID number again?"
+team_id = gets.to_i
+game_outcome(db, win, team_comment, team_id)
+puts "Thank you for playing College Ultimate Frisbee!"
+
+
 
 # require 'sqlite3'
 # require 'faker'
