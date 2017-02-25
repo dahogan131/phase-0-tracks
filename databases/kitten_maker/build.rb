397471ecda50c7396e5195b9c49773faf0a61e06
Lia <dahogan131@gmail.com>
2017-02-24 21:29:49 -0800

    Debugging.

--- databases/kitten_maker/build.rb
+++ databases/kitten_maker/build.rb
@@ -1,5 +1,3 @@
-
-
 require 'sqlite3'
 require 'faker'
 
@@ -23,6 +21,8 @@ create_teams_table = <<-SQL
 	)
 SQL
 
+
+
 db.execute(create_teams_table)
 db.execute(create_player_table)
 
@@ -30,23 +30,23 @@ def create_team(db, team_name)
 	db.execute("INSERT INTO teams (team_name) VALUES (?)", [team_name])
 end
 
+def create_players(db, name, age, team_id)
+	db.execute("INSERT INTO players (name, age, team_id) VALUES (?, ?, ?)", [name, age, team_id])
+end
+
 user_input = ""
 while user_input != "done"
 # 2.times do |team_name|
-	puts "Please enter Team Name."
+	puts "Please enter Team Name:"
 	team_name = gets.chomp
 	create_team(db, team_name)
 	puts "please enter any key to continue submitting teams. when done, enter 'done'"
 	user_input = gets.chomp
 end
 
-def create_players(db, name, age, team_id)
-	db.execute("INSERT INTO players (name, age, team_id) VALUES (?, ?, ?)", [name, age, team_id])
-end
 
-
-user_input = ""
-while user_input != "done"
+user_input2 = ""
+while user_input2 != "done"
 	#puts "press any key and enter to start or enter 'done' when finished."
 	#user_input = gets.chomp
 	puts "Hello, what is your age?"
@@ -56,8 +56,75 @@ while user_input != "done"
   create_players(db, Faker::Name.name, age, team_id)
   	puts "Thank you #{Faker::Name.name}!"
   	puts "Press any key and enter to continue. Enter 'done' when finished."
-  	user_input = gets.chomp
+  	user_input2 = gets.chomp
 end
+
+# require 'sqlite3'
+# require 'faker'
+
+# db = SQLite3::Database.new("ultimate_frisbee.db")
+# db.results_as_hash = true
+
+# create_player_table = <<-SQL 
+# 	CREATE TABLE IF NOT EXISTS players(
+# 		id INTEGER PRIMARY KEY, 
+# 		name VARCHAR (255), 
+# 		age INT, 
+# 		team_id INT,
+# 		FOREIGN KEY (team_id) REFERENCES teams(id)
+# 	)
+# SQL
+
+# create_teams_table = <<-SQL 
+# 	CREATE TABLE IF NOT EXISTS teams(
+# 		id INTEGER PRIMARY KEY,
+# 		team_name VARCHAR (255),
+# 		wins INT,
+# 		losses INT
+# 	)
+# SQL
+
+
+
+# db.execute(create_teams_table)
+# db.execute(create_player_table)
+
+# def create_team(db, team_name, wins, losses)
+# 	db.execute("INSERT INTO teams (team_name, wins, losses) VALUES (?, ?, ?)", [team_name, wins, losses])
+# end
+
+# def create_players(db, name, age, team_id)
+# 	db.execute("INSERT INTO players (name, age, team_id) VALUES (?, ?, ?)", [name, age, team_id])
+# end
+
+# user_input = ""
+# while user_input != "done"
+# # 2.times do |team_name|
+# 	total_games = 10
+# 	puts "Please enter Team Name:"
+# 	team_name = gets.chomp
+# 	puts "Please enter your number of wins:"
+# 	wins = gets.to_i
+# 	losses = total_games - wins
+# 	create_team(db, team_name, wins, losses)
+# 	puts "please enter any key to continue submitting teams. when done, enter 'done'"
+# 	user_input = gets.chomp
+# end
+
+
+# user_input2 = ""
+# while user_input2 != "done"
+# 	#puts "press any key and enter to start or enter 'done' when finished."
+# 	#user_input = gets.chomp
+# 	puts "Hello, what is your age?"
+# 	age = gets.to_i
+# 	puts "What is your Team ID number?"
+# 	team_id = gets.to_i 
+#   create_players(db, Faker::Name.name, age, team_id)
+#   	puts "Thank you #{Faker::Name.name}!"
+#   	puts "Press any key and enter to continue. Enter 'done' when finished."
+#   	user_input2 = gets.chomp
+# end
 
 
 
