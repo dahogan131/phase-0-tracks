d2dd10f8feb16052338510474bfa404ce3ba77d0
Lia <dahogan131@gmail.com>
2017-03-04 13:27:44 -0800

    Change border and text color in CSS and create event changer in JS.

--- web_dev/client_side_js/vanilla.js
+++ web_dev/client_side_js/vanilla.js
@@ -12,10 +12,21 @@ var show_platform = function(){
 	platform.hidden = false
 };
 
-var color = function(){
+var color_intro = function(){
 	var color = document.getElementById("intro")
-	color.style.color = "blue";
+	color.style.backgroundColor = "#339933";
 };
+
+var color_header = function(){
+	var color = document.getElementById("hello")
+	color.style.backgroundColor = "#339933";
+};
+
+var color_platform = function(){
+	var color = document.getElementById("platform")
+	color.style.backgroundColor = "#339933";
+};
+
 
 
 // var platform_button = getElementById("platform-button");
@@ -38,8 +49,12 @@ var show_picture = function(){
 var platform_button = document.getElementById("platform-button");
 var picture_button = document.getElementById("foot-button");
 var hover_color = document.getElementById("intro")
+var hover_color2 = document.getElementById("hello")
+var hover_color3 = document.getElementById("platform")
 
 picture_button.addEventListener("click", show_picture);
 platform_button.addEventListener("click", show_platform);
-hover_color.addEventListener("mouseover", color);
+hover_color.addEventListener("mouseover", color_intro);
+hover_color2.addEventListener("mouseover", color_header);
+hover_color3.addEventListener("mouseover", color_platform);
 
